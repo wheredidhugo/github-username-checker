e9f97dcedd2864dff86b68b39d0c0896d1b006e2
Hugo <bluegopvpyt@gmail.com>
2021-12-18 18:49:41 +0100

    Added authentification + cleaned some stuff

--- main.py
+++ main.py
@@ -3,61 +3,77 @@ import string
 import random
 import os
 import platform
-
-def clConsole():
-  if platform.system() == "Linux" or "Darwin":
-    os.system('clear')
-  else:
-    os.system('cls')
-
-clConsole()
+import json
 
 success = None;
 
-for x in range(999):
-  print("How many letters do you want your usernames to be? ")
-  lUsername = int(input())
-  if lUsername > 39:
-    print("You can have a maximum of 39 letters in your username, try again.")
-  elif lUsername == 0:
-    print("You can't have no username, try again.")
-  else:
-    break
-
-def getRandomString(size=lUsername):
-  return ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(size))
+sFile = "settings.json";
 
 def writeName(name):
-  f = open("users.txt", "a")
-  f.write(name)
-  f.write(", ")
-  print(name, ">>> users.txt")
-  f.close()
+    f = open("users.txt", "a")
+    f.write(name + ", ")
+    print(name, ">>> users.txt")
+    f.close()
 
-rLimit = requests.get('https://api.github.com/users/').headers['x-ratelimit-remaining']
-rLimit = int(rLimit)
+def clConsole():
+  if platform.system() == "Linux" or "Darwin":
+      os.system('clear');
+  else:
+      os.system('cls');
+
+with open(sFile, "r", encoding="utf-8") as i:
+    data = json.load(i);
+
+token = data["token"];
+sLoop = data["stopLoop"];
+
+headers = {"Authorization": 'token ' + token};
+
+clConsole()
+
+for x in range(999):
+    print("How many letters do you want your usernames to be? ")
+    lUsername = int(input())
+    if lUsername > 39:
+        print("You can have a maximum of 39 letters in your username, try again.")
+    elif lUsername == 0:
+        print("You can't have no username, try again.")
+    else:
+        break
+
+
+def getRandomString(size=lUsername):
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for i in range(size))
+
+if token == "":
+  rLimit = requests.get('https://api.github.com/users/').headers['x-ratelimit-remaining']
+  rLimit = int(rLimit)
+else:
+  rLimit = requests.get('https://api.github.com/users/', headers=headers).headers['x-ratelimit-remaining']
+  rLimit = int(rLimit)
 
 clConsole()
 
 for i in range(rLimit):
   if rLimit == 0:
-    print("Sorry, you have been ratelimited, try again later.")
-    break;
+      print("Sorry, you have been ratelimited, try again later.")
+      break
 
   RandomString = getRandomString()
   r = requests.get('https://api.github.com/users/' + RandomString)
   jsonResponse = r.json()
 
-  try: 
+  try:
     if jsonResponse["message"] == "Not Found":
       print(RandomString, "isn't taken.")
       writeName(RandomString)
-      break;
+      if sLoop == "true":
+        break;
   except:
     if jsonResponse["login"] == RandomString:
       print(RandomString, "is taken.")
-      rLimit = rLimit - 1; 
-      success = 1;
+      rLimit = rLimit - 1
+      success = 1
 
 if rLimit == 0:
   print("Sorry, you have been ratelimited, try again later.")
